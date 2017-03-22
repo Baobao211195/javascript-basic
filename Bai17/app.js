1f094f94001ec0b73afd2118a3fb5025588b9574
Oanhpv <oanhmrphamvan@gmail.com>
2017-03-22 23:07:34 +0700

    strict mode

--- Bai17/app.js
+++ Bai17/app.js
@@ -1,8 +1,19 @@
 // ======strict mode trong js================
-x = 3.14;       // This will not cause an error. 
-myFunction();
+// x = 3.14;       // This will not cause an error. 
+// myFunction();
 
-function myFunction() {
-   "use strict";
-    y = 3.14;   // This will cause an error
+// function myFunction() {
+//    "use strict";
+//     y = 3.14;   // This will cause an error
+// }
+'use strict';
+console.log("=======vi du 1======");
+function foo(){
+    var x, y;
+    x =1;
+    y = 2;
+    z = x + y; // z is undefined
 }
+foo();
+console.log("=======vi du 2======");
+// trong with la mot doi tuong.
