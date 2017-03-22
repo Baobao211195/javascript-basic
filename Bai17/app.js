a6ddc240e23d9a482a48ce487a6cb67cb6c9e033
Oanhpv <oanhmrphamvan@gmail.com>
2017-03-22 21:46:41 +0700

    strict mode trong js

---
+++ Bai17/app.js
@@ -0,0 +1,8 @@
+// ======strict mode trong js================
+x = 3.14;       // This will not cause an error. 
+myFunction();
+
+function myFunction() {
+   "use strict";
+    y = 3.14;   // This will cause an error
+}
